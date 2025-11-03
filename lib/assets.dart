eaee0ec13dfbc492d45f9ebd0dafc318eca35e34
nightowlapp <platform.nightowl@gmail.com>
2025-11-03 21:36:08 +0100

    added correct logo

---
+++ lib/assets.dart
@@ -0,0 +1,16 @@
+class ImagePaths {
+  static String root = 'assets/nightowl';
+  static String flags = 'assets/flags';
+
+  static String logo = logoLeft;
+  static String logoLeft = '$root/logo_left.png';
+  static String logoUp = '$root/logo_up.png';
+  static String logoRight = '$root/logo_right.png';
+  static String logoDown = '$root/logo_down.png';
+
+
+  static String ukFlag = '$flags/uk.png';
+  static String dkFlag = '$flags/dk.png';
+
+
+}
